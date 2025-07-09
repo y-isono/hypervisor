22ff373ffcad92a4e5df766cd684e741d48dcd74
wadatti <22770733+wadatti@users.noreply.github.com>
2025-07-09 14:24:34 +0000

    Refactoring

--- src/kernel/util.h
+++ src/kernel/util.h
@@ -4,5 +4,5 @@ static inline void mmio_write(uint32_t reg, uint32_t data);
 static inline uint32_t mmio_read(uint32_t reg);
 void uart_init();
 void uart_putc(unsigned char c);
-void uart_puts(const char* str);
+void uart_puts(const char *str);
 unsigned char uart_getc();
